d380d442b8abf7b0f2a8bf42b40deb8762f8e606
ChocoFanta-01 <ljh970512@naver.com>
2026-04-29 21:48:57 +0900

    fix: eslint 오류 수정

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -13,6 +13,7 @@ export interface JwtPayload {
 
 // Request 타입에 user 필드 추가 (TypeScript 타입 확장)
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
       user?: JwtPayload;
